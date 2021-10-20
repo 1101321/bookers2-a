613a9f54a06069962269ad0cda481b148846f8c8
1101321 <hcri0909@gmail.com>
2021-10-20 06:01:08 +0000

    [Update] ユーザ一覧に全てのユーザを表示・マイページに自分の投稿のみを表示

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -30,6 +30,11 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
     @user = current_user
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
   end
   
   def update
