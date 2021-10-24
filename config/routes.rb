4ef42979fe81720fa038a2e18b467f741f362d39
1101321 <hcri0909@gmail.com>
2021-10-24 11:02:12 +0000

    [Start] 応用課題3

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
   get '/books' => 'books#new', as: 'new_book'
   get '/books' => 'books#show'
   
-  
   resources :users, only: [:show, :index, :edit, :update]
-  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
