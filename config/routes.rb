4b15de5f8ee7996b95d999508d573c72e7c80600
1101321 <hcri0909@gmail.com>
2021-10-24 12:24:22 +0000

    [Add] ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about'
   resources :books, only: [:create, :index, :show, :destroy, :edit, :update]do
     resource :favorites, only: [:create, :destroy]
+    resource :book_comments, only: [:create, :destroy]
   end
   get '/books' => 'books#new', as: 'new_book'
   get '/books' => 'books#show'
