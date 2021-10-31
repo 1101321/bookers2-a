57f0e570fb0a8a4cf5444d3f98b7a0c59e14a19a
1101321 <hcri0909@gmail.com>
2021-10-31 09:01:49 +0000

    [Add] Relationshipモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_24_121307) do
+ActiveRecord::Schema.define(version: 2021_10_31_085815) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -36,6 +36,13 @@ ActiveRecord::Schema.define(version: 2021_10_24_121307) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -52,4 +59,3 @@ ActiveRecord::Schema.define(version: 2021_10_24_121307) do
   end
 
 end
-
