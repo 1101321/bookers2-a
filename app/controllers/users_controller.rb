e7b3c0cbf7c6c135777534bc8b4e1e1c9a8c2e5d
1101321 <hcri0909@gmail.com>
2021-10-31 11:30:49 +0000

    [Add] フォロー・フォロワー一覧画面の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
-  
+
   def show
     @user = User.find(params[:id])
     @books = Book.all
     @book = Book.new
-    
+
   end
-  
+
   def edit
     @user = User.find(params[:id])
     if @user == current_user
@@ -15,13 +15,13 @@ class UsersController < ApplicationController
       redirect_to user_path(current_user.id)
     end
   end
-  
+
   def index
     @users = User.all
     @user = current_user
     @book = Book.new
   end
-  
+
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
@@ -31,12 +31,11 @@ class UsersController < ApplicationController
       render :edit
     end
   end
-  
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
-  
+
 end
