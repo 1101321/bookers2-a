9038f090c1bb72acf3444b1d1462d6ad15d3ff64
1101321 <hcri0909@gmail.com>
2021-10-31 09:14:41 +0000

    [Add] アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,10 @@ class User < ApplicationRecord
   validates :email, uniqueness: true
   validates :name, uniqueness: true, length: { minimum:2, maximum: 20 }
   validates :introduction, length: { maximum: 50 }
-
+  
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  has_many :followings, through: :relationships, source: :followed
 end
