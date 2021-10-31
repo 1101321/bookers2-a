11d5c48a4b3fa21ee951b1776be4a807d7a5376b
1101321 <hcri0909@gmail.com>
2021-10-31 09:39:32 +0000

    [Add] ルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,11 @@ Rails.application.routes.draw do
   get '/books' => 'books#new', as: 'new_book'
   get '/books' => 'books#show'
   
-  resources :users, only: [:show, :index, :edit, :update]
+  resources :users, only: [:show, :index, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
