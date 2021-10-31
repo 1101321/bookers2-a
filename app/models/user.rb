4d88d481a65f8d8774b4501c39c4117519975249
1101321 <hcri0909@gmail.com>
2021-10-31 09:21:03 +0000

    [Add] Userモデルにメソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,17 @@ class User < ApplicationRecord
   
   has_many :followers, through: :reverse_of_relationships, source: :follower
   has_many :followings, through: :relationships, source: :followed
+  
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  def following?(user)
+    followings.include?(user)
+  end
+  
 end
