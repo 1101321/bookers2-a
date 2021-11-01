2a2c71542b857a3ab9948674d8052bcd7870087c
1101321 <hcri0909@gmail.com>
2021-11-01 12:14:03 +0000

    [Fix] searchアクションの修正

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,11 @@
 class SearchesController < ApplicationController
-  @model = params["search"]["model"]
-  @value = params["search"]["value"]
-  @how = params["search"]["how"]
-  @dates = search_for(@how, @model, @value)
+  
+  def search
+    @model = params["search"]["model"]
+    @value = params["search"]["value"]
+    @how = params["search"]["how"]
+    @dates = search_for(@how, @model, @value)
+  end
+  
 end
+
