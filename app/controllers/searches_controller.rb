bc80e971d6ab712ccfac01bcf8569ce4959985fa
1101321 <hcri0909@gmail.com>
2021-11-01 12:10:18 +0000

    [Add] searchアクションの作成

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,4 +1,6 @@
 class SearchesController < ApplicationController
-
-  
+  @model = params["search"]["model"]
+  @value = params["search"]["value"]
+  @how = params["search"]["how"]
+  @dates = search_for(@how, @model, @value)
 end
