e170e9209085a50c13b5b2c1c9e2f317ac12614e
1101321 <hcri0909@gmail.com>
2021-11-09 10:46:53 +0000

    [Add] Messageモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_104147) do
+ActiveRecord::Schema.define(version: 2021_11_09_104446) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -43,6 +43,16 @@ ActiveRecord::Schema.define(version: 2021_11_09_104147) do
     t.integer "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "room"
+    t.string "references"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
